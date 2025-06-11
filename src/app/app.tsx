f836ac605e9e1349a422e940ccb0c3f5abc7c310
omnia <Omniaabdelnasser067@gmail.com>
2025-06-11 14:14:00 +0300

    omnia

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -11,7 +11,6 @@ export default function App() {
         <Route path="/details/:projectId" element={<Details />} />
       </Routes>
 
-      {/* زر الرجوع للأعلى */}
       <Scroll />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./app/app";
 import Providers from "./app/providers";
 import "./index.css";
-import "./transition.css";
+// import "./transition.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./app/providers/theme-provider";
 const queryClient = new QueryClient();
