a4c76366fa81887f4121941809bb64cb9df237e2
Abdallah-Alqiran <abdallahalqiran765@gmial.com>
2025-06-24 17:38:14 +0300

    add arrows to projects and coruses sections

--- src/app/pages/courses.tsx
+++ src/app/pages/courses.tsx
@@ -3,6 +3,8 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from "@/shared/components/ui/carousel";
 import { motion } from "framer-motion";
 import { GraduationCap } from "lucide-react";
@@ -23,36 +25,40 @@ const Courses = () => {
         Courses
       </h1>
 
-      <Carousel>
-        <CarouselContent className="-ml-4">
-          {data?.courses.map((course,index) => (
-            <CarouselItem
-              key={course.id || index}
-              className="pl-4 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
-            >
-              <motion.div
-                className="h-full rounded-2xl 
-                 dark:text-gray-300 shadow-lg p-6 hover:scale-[1.02] 
-                 transition-transform duration-300
-                 border-2 border-[#209191] "
-                initial={{ opacity: 0, scale: 0.95 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ delay: index * 0.1, duration: 0.93 }}
-                viewport={{ once: true }}
+      <div className="relative w-full px-8">
+        <Carousel className="w-full">
+          <CarouselPrevious />
+          <CarouselContent className="-ml-4">
+            {data?.courses?.map((course,index) => (
+              <CarouselItem
+                key={course.id || index}
+                className="pl-4 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
               >
-                <div className="flex items-center gap-2 mb-3 text-[#209191]  ">
-                  <GraduationCap className="w-8 h-8 dark:text-white drop-shadow-md" />
-                  <h2 className=" text-md md:text-xl font-bold">{course.courseName}</h2>
-                </div>
-                <p className="text-md md:text-lg font-semibold
-                 leading-relaxed text-gray-600 dark:text-gray-300 ">
-                  {course.courseDescription}
-                </p>
-              </motion.div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+                <motion.div
+                  className="h-full rounded-2xl 
+                   dark:text-gray-300 shadow-lg p-6 hover:scale-[1.02] 
+                   transition-transform duration-300
+                   border-2 border-[#209191] "
+                  initial={{ opacity: 0, scale: 0.95 }}
+                  whileInView={{ opacity: 1, scale: 1 }}
+                  transition={{ delay: index * 0.1, duration: 0.93 }}
+                  viewport={{ once: true }}
+                >
+                  <div className="flex items-center gap-2 mb-3 text-[#209191]  ">
+                    <GraduationCap className="w-8 h-8 dark:text-white drop-shadow-md" />
+                    <h2 className=" text-md md:text-xl font-bold">{course.courseName}</h2>
+                  </div>
+                  <p className="text-md md:text-lg font-semibold
+                   leading-relaxed text-gray-600 dark:text-gray-300 ">
+                    {course.courseDescription}
+                  </p>
+                </motion.div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselNext />
+        </Carousel>
+      </div>
     </section>
   );
 };
